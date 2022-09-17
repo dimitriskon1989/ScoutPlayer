3d3cf9cf177c40d609fbff7155b2a287cdc4d3a0
dimitriskon1989 <45177291+dimitriskon1989@users.noreply.github.com>
2022-09-17 11:40:15 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,7 @@
 
 export const environment = {
   firebase: {
-  apiKey: "AIzaSyDwjeZ3oT6LVgyW9l81qKtayqC-Y1Jd15s",
-  authDomain: "scoutplayer-8b765.firebaseapp.com",
-  projectId: "scoutplayer-8b765",
-  storageBucket: "scoutplayer-8b765.appspot.com",
-  messagingSenderId: "417811224846",
-  appId: "1:417811224846:web:eec3f803d50d7cc3fbdf5f"
+  
   },
   production: false
 };
